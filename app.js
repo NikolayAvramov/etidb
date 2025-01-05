967b8d35569dc4e5b5c8d0de594b1e8eaf236780
Nikolay <106226408+NikolayAvramov@users.noreply.github.com>
2025-01-05 16:18:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
 app.use((error, req, res, next) => {
     res.status(error.status || 500).json({
         error: {
-            message: error.message,
+            message: "test",
         },
     });
 });
